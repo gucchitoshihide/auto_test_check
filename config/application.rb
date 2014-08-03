8ac2cc9e3bd112c4ef7a5c2b1f26e6787e04c4b6
Yusuke Matsukawa <mat27y@gmail.com>
2014-08-03 22:09:00 +0900

    Moved model to make physical and logical design.

--- config/application.rb
+++ config/application.rb
@@ -15,15 +15,12 @@ Bundler.require(*Rails.groups)
 
 module Las
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # http://qiita.com/somewhatgood@github/items/32b916ad91d556091d9d
+    config.autoload_paths += %W(#{config.root}/app/models/logical)
+    config.autoload_paths += %W(#{config.root}/app/models/physical)
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+
     # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
