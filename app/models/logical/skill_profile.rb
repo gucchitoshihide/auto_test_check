00e3bd793497fa45c06960c970e447ba6fbb7db0
Yusuke Matsukawa <mat27y@gmail.com>
2014-08-09 22:33:03 +0900

    Add post on skill profile.

--- app/models/logical/skill_profile.rb
+++ app/models/logical/skill_profile.rb
@@ -6,4 +6,25 @@ class SkillProfile < ActiveRecord::Base
     Article.order('created_at DESC').limit(list_num)
   }
 
+  class << self
+    def submit(params)
+      profile = Article.new(params)
+      if profile.save
+        SkillProfile.new(article_id: profile.id).save
+      else
+        raise ValidationError, active_model_errors_to_string(profile)
+      end
+    end
+
+    def format_error_message(error_message)
+      error_message.split(Settings[:back][:model][:error][:seperate])
+    end
+
+    private
+
+    def active_model_errors_to_string(errored_model_obj)
+      errored_model_obj.errors.messages.values.flatten.join(Settings[:back][:model][:error][:seperate])
+    end
+  end
+
 end
