d8e7b0536c8a200b8ec17864230828432027d326
Yusuke Matsukawa <mat27y@gmail.com>
2014-08-11 14:12:19 +0900

    Add comment logic.

--- app/models/logical/comment.rb
+++ app/models/logical/comment.rb
@@ -4,6 +4,19 @@ class Comment < ActiveRecord::Base
   include RelationComment
   include ValidationComment
 
+  scope :followed?, ->(article_id) {
+    ArticleComment.where('article_id = ?', article_id).present?
+  }
+
+  scope :show_all, ->(article_id) {
+    related_comment_id = ArticleComment.where('article_id = ?', article_id).map { |record| record.comment_id }
+    related_comment_id.map { |comment_id| self.find_by_id(comment_id) }
+  }
+
+  scope :num_followed, ->(article_id) {
+    ArticleComment.where('article_id = ?', article_id).count
+  }
+
   class << self
 
     def submit_on_week_report(params, article_id)
