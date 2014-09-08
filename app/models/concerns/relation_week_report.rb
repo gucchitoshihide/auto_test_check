8293defbabc8ea5771c07f1f2a91a30c5a5e728d
Yusuke Matsukawa <mat27y@gmail.com>
2014-09-08 23:12:35 +0900

    Removed no use file.

--- app/models/concerns/relation_week_report.rb
+++
@@ -1,9 +0,0 @@
-module RelationWeekReport
-  extend ActiveSupport::Concern
-
-  included do
-    has_many :users
-    has_many :article_week_reports
-    has_many :articles, dependent: :destroy, through: :article_week_reports
-  end
-end
